e2c01a0b7b44cb46a4fda15dcf4b4cd9936cdbea
NoahKusaba <noahskusaba@gmail.com>
2023-04-30 19:03:37 -0400

    Update to GoogleMaps

--- AWS_Airflow_Webscraper/transformations/coordinate.py
+++ AWS_Airflow_Webscraper/transformations/coordinate.py
@@ -1,39 +1,15 @@
-import requests
 from settings import CordKey
+import googlemaps
+#GoogleMaps Geocode APi
 
-#TOMTOM Geocode APi
+gmaps = googlemaps.Client(key = CordKey)
+
 def returnCoordinate(name):
 
     for event in name:
-        if event["City"] == "Bangkok":
-            event.update({"Coordinates":[13.8757,100.66738]})  # exception handeling for Lumpinee Boxing Stadium
-        elif event["City"] == "Barueri":
-            event.update({"Coordinates":[-23.5070384,-46.8679179]}) 
-        elif 'CANCELLED' in [ event['City'], event['Country'] ]:
-            pass
+        geocode_query = event['City'] + ' '+ event['Country'] + ' '+ event['Venue']
+        geocode_result = gmaps.geocode(geocode_query)
+        cords = list(geocode_result[0]['geometry']['location'].values())
+        event.update({"Coordinates":cords})
 
-        else: 
-            if event["City"] == "Wein":
-                event["City"] = "Vienna"
-            url ="https://api.tomtom.com/search/2/geocode/"
-
-            #url = "https://api.radar.io/v1/search/autocomplete?query=" failed radar api
-            if event["Type"] == "bjj":
-                url += event["City"]+ ", " + event["Country"] +".json" +f"?key={CordKey}"
-            else:
-                url+= event["Venue"] +", "+event["City"]+ ", " + event["Country"] +".json" +f"?key={CordKey}"
-
-            response = requests.get(url)
-        
-            if response.status_code !=200:
-                print("Error GeoCode")
-                print(response)
-                exit()        
-
-            data= response.json()
-            try:
-                cords = list(data["results"][0]["position"].values())
-                event.update({"Coordinates":cords})
-            except IndexError:
-                breakpoint()        
     return name
